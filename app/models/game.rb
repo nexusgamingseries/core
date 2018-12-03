8fab64b814fd47159dd3352935352e132ec62635
Brad Adams <badams@appcolony.ca>
2018-12-02 21:06:53 -0700

    Fixed relation typo

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,9 @@
 class Game < ApplicationRecord
 
   belongs_to :match
-  has_one    :away_team, :through :matches
-  has_one    :home_team, :through :matches
-  has_one    :winner,    :through :matches
-  has_one    :loser,     :through :matches
+  has_one    :away_team, through: :matches
+  has_one    :home_team, through: :matches
+  has_one    :winner,    through: :matches
+  has_one    :loser,     through: :matches
 
 end
