6baa170bbcf0d59d854cbb8deb64d9207557f14b
Carlos Lazo <carlos.jose.lazo@gmail.com>
2018-12-09 13:15:31 -0500

    Hound (Rubocop) and Travis (CI) fixes.

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -2,7 +2,7 @@
 RSpec.describe PagesHelper, type: :helper do
   describe 'string concat' do
     it 'concats two strings with spaces' do
-      expect(helper.concat_strings('this', 'that')).to eq("this   that")
+      expect(helper.concat_strings('this', 'that')).to eq('this that')
     end
   end
 end
