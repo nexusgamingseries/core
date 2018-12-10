fb3e0185d76434587f01bfd2d291db3eaecbc3cc
Michael Michalak <mike-michalak@utulsa.edu>
2018-12-09 22:25:30 -0800

    Initial commit to support creating Teams.
    Includes a basic form to submit a Team Name and Logo URL, which will save to DB.
    Includes an index to view all teams, and a page to view a specific team by name.
    
    Does not include any style/formatting.
    Does not include any tests.
    Does not include any polish whatsoever.

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,22 @@
+class TeamsController < ApplicationController
+  def index
+  	@team = Team.all
+  end
+
+  def show
+    @team = Team.where(:name => params[:id]).first
+  end
+
+  def new
+  	@team = Team.new
+  end
+
+  def create
+  	@team = Team.new(params[:team].permit!)
+    if @team.save
+        redirect_to @team, alert: "Team created successfully."
+    else
+        redirect_to new_user_path, alert: "Error creating team."
+    end
+  end
+end
