8fcfd2f1b03d4dac0c1033f88a1f270ae59ecd0d
Carlos Lazo <carlos.jose.lazo@gmail.com>
2018-12-12 20:49:45 -0500

    :art: Rubocop warnings; remove empty specs.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class TeamsController < ApplicationController
   def index
-  	@team = Team.all
+    @team = Team.all
   end
 
   def show
-    @team = Team.where(:name => params[:id]).first
+    @team = Team.where(name: params[:id]).first
   end
 
   def new
-  	@team = Team.new
+    @team = Team.new
   end
 
   def create
-  	@team = Team.new(params[:team].permit!)
+    @team = Team.new(params[:team].permit!)
     if @team.save
-        redirect_to @team, alert: "Team created successfully."
+        redirect_to @team, alert: 'Team created successfully.'
     else
-        redirect_to new_user_path, alert: "Error creating team."
+        redirect_to new_user_path, alert: 'Error creating team.'
     end
   end
 end
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe TeamsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
+  describe 'GET #show' do
+    it 'returns http success' do
       get :show
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
+  describe 'GET #create' do
+    it 'returns http success' do
       get :create
       expect(response).to have_http_status(:success)
     end
